935b546586ec33fb5af7d02f07ba26f0c2bef072
eb <eb@2bros.dev>
2020-12-04 03:38:28 -0600

    day 3 part 1 solution

--- days/day03/main.cpp
+++ days/day03/main.cpp
@@ -2,7 +2,10 @@
 #include <aoc2020/aoc2020.hpp>
 
 #include <fmt/core.h>
+#include <range/v3/all.hpp>
+
 #include <fstream>
+#include <string>
 
 int main(int argc, const char** argv)
 {
@@ -11,6 +14,19 @@ int main(int argc, const char** argv)
     }
 
     std::ifstream ifs{argv[1]};
+
+    std::string str;
+    int         pos     = 0;
+    int         counter = 0;
+
+    while (std::getline(ifs, str)) {
+        if (str[pos % str.length()] == '#')
+            ++counter;
+
+        pos += 3;
+    }
+
+    fmt::print("Part 1 Solution: {}\n", counter);
 
     return 0;
 }
