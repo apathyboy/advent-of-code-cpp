ea754dcdf07d954dd510d36682c248a5986d5ecb
eb <eb@2bros.dev>
2020-12-04 04:06:53 -0600

    day 3 solution 2

--- days/day03/main.cpp
+++ days/day03/main.cpp
@@ -16,17 +16,31 @@ int main(int argc, const char** argv)
     std::ifstream ifs{argv[1]};
 
     std::string str;
-    int         pos     = 0;
-    int         counter = 0;
+    int         slope1 = 1, slope2 = 3, slope3 = 5, slope4 = 7;
+    int         count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0;
+    int         depth = 0;
 
     while (std::getline(ifs, str)) {
-        if (str[pos % str.length()] == '#')
-            ++counter;
+        if (str[(depth * slope1) % str.length()] == '#')
+            ++count1;
 
-        pos += 3;
+        if (str[(depth * slope2) % str.length()] == '#')
+            ++count2;
+
+        if (str[(depth * slope3) % str.length()] == '#')
+            ++count3;
+
+        if (str[(depth * slope4) % str.length()] == '#')
+            ++count4;
+
+        if (depth % 2 == 0 && str[(depth / 2) % str.length()] == '#')
+            ++count5;
+
+        ++depth;
     }
 
-    fmt::print("Part 1 Solution: {}\n", counter);
+    fmt::print("Part 1 Solution: {}\n", count2);
+    fmt::print("Part 2 Solution: {}\n", count1 * count2 * count3 * count4 * count5);
 
     return 0;
 }
