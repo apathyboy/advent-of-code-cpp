b76776f4994492c3b649dcb7b9a12fbe82932c4f
eb <eb@2bros.dev>
2020-12-06 03:10:19 -0600

    day 6 stub

---
+++ days/day06/main.cpp
@@ -0,0 +1,15 @@
+
+#include <fmt/core.h>
+
+#include <fstream>
+
+int main(int argc, const char** argv)
+{
+    if (argc != 2) {
+        fmt::print("USAGE: day05 <input_file_path>\n");
+    }
+
+    std::ifstream ifs{argv[1]};
+
+    return 0;
+}
