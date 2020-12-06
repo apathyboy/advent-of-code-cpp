130ea3a7ed0ac5ffbac3b33d249f817bf78ad7d0
eb <eb@2bros.dev>
2020-12-06 04:44:16 -0600

    day 6 part 2 solution

--- days/day06/main.cpp
+++ days/day06/main.cpp
@@ -1,5 +1,6 @@
 
 #include <fmt/core.h>
+#include <range/v3/all.hpp>
 
 #include <fstream>
 #include <set>
@@ -11,23 +12,53 @@ int main(int argc, const char** argv)
         fmt::print("USAGE: day06 <input_file_path>\n");
     }
 
-    std::ifstream ifs{argv[1]};
+    {
+        std::ifstream ifs{argv[1]};
 
-    int sum_of_counts = 0;
+        int sum_of_counts = 0;
 
-    std::set<char> questions;
+        std::set<char> questions;
 
-    std::string str;
-    while (std::getline(ifs, str)) {
-        if (str.length() == 0) {
-            sum_of_counts += static_cast<int>(questions.size());
-            questions.clear();
+        std::string str;
+        while (std::getline(ifs, str)) {
+            if (str.length() == 0) {
+                sum_of_counts += static_cast<int>(questions.size());
+                questions.clear();
+            }
+
+            questions.insert(str.begin(), str.end());
         }
 
-        questions.insert(str.begin(), str.end());
+        fmt::print("Part 1 Solution: {}\n", sum_of_counts);
     }
 
-    fmt::print("Part 1 Solution: {}\n", sum_of_counts);
+    {
+        std::ifstream ifs{argv[1]};
+
+        int sum_of_counts = 0;
+
+        std::string str;
+        std::string questions;
+
+        std::getline(ifs, questions);
+        ranges::sort(questions);
+
+        while (std::getline(ifs, str)) {
+            if (str.length() == 0) {
+                sum_of_counts += static_cast<int>(questions.size());
+                std::getline(ifs, questions);
+                ranges::sort(questions);
+            }
+            else {
+                ranges::sort(str);
+
+                questions = ranges::views::set_intersection(questions, str)
+                            | ranges::to<std::string>;
+            }
+        }
+
+        fmt::print("Part 2 Solution: {}\n", sum_of_counts);
+    }
 
     return 0;
 }
