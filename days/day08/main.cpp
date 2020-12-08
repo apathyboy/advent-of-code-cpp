6e2a75692ca7567691bfdac2139d37690909ed9c
eb <eb@2bros.dev>
2020-12-08 00:02:28 -0600

    added day 8 stub

---
+++ days/day08/main.cpp
@@ -0,0 +1,13 @@
+
+#include <fmt/core.h>
+
+#include <fstream>
+
+int main()
+{
+    fmt::print("Advent of Code 2020 - Day 08\n");
+
+    std::ifstream ifs{"days/day08/example.txt"};
+
+    return 0;
+}
