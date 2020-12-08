a80b8fc9b33bb9eaefc504362354c2027ad9dff0
eb <eb@2bros.dev>
2020-12-08 11:52:45 -0600

    day 8 cleanup and tests

--- days/day08/main.cpp
+++ days/day08/main.cpp
@@ -3,20 +3,18 @@
 #include <range/v3/all.hpp>
 
 #include <fstream>
+#include <map>
 #include <set>
 
 namespace rv = ranges::views;
 
-struct instruction {
-    std::string name;
-    int         amt;
-};
-
 class game_console {
 public:
+    enum class OP_TYPE : int { NOP = 0, JMP = 1, ACC = 2 };
+
     struct instruction {
-        std::string name;
-        int         amt;
+        OP_TYPE op;
+        int     amt;
     };
 
     using program = std::vector<instruction>;
@@ -27,8 +25,8 @@ public:
     };
 
 public:
-    game_console(std::vector<instruction> program)
-        : program_{std::move(program)}
+    game_console(const std::vector<instruction>& program)
+        : program_{program}
     {
     }
 
@@ -36,40 +34,39 @@ public:
     {
         std::set<int> cache;
 
-        while (!inf_loop_detected_ && !program_terminate_) {
+        while (!inf_loop_detected_) {
             auto& i = program_[next_instruction_];
 
-            if (i.name == "acc") {
-                accumulator_ += i.amt;
-                ++next_instruction_;
-            }
-            else if (i.name == "jmp") {
-                next_instruction_ += i.amt;
-            }
-            else if (i.name == "nop") {
-                ++next_instruction_;
+            switch (i.op) {
+                case OP_TYPE::NOP: {
+                    ++next_instruction_;
+                } break;
+                case OP_TYPE::JMP: {
+                    next_instruction_ += i.amt;
+                } break;
+                case OP_TYPE::ACC: {
+                    accumulator_ += i.amt;
+                    ++next_instruction_;
+                } break;
             }
 
-            if (next_instruction_ >= program_.size()) {
-                program_terminate_ = true;
-            }
-            else if (cache.contains(next_instruction_)) {
-                inf_loop_detected_ = true;
-            }
-            else {
+            if (next_instruction_ >= program_.size())
+                break;
+
+            inf_loop_detected_ = cache.contains(next_instruction_);
+
+            if (!inf_loop_detected_)
                 cache.emplace(next_instruction_);
-            }
         }
 
         return {accumulator_, inf_loop_detected_};
     }
 
 private:
-    program program_;
-    int     accumulator_       = 0;
-    int     next_instruction_  = 0;
-    bool    program_terminate_ = false;
-    bool    inf_loop_detected_ = false;
+    const program& program_;
+    int            accumulator_       = 0;
+    int            next_instruction_  = 0;
+    bool           inf_loop_detected_ = false;
 };
 
 int part1(const game_console::program& p)
@@ -81,21 +78,21 @@ int part1(const game_console::program& p)
     return acc;
 }
 
-int part2(const game_console::program& p)
+int part2(game_console::program p)
 {
     bool found       = false;
     int  accumulator = 0, check = 0;
 
     while (!found) {
-        while (p[check].name != "nop" && p[check].name != "jmp")
+        while (p[check].op != game_console::OP_TYPE::NOP
+               && p[check].op != game_console::OP_TYPE::JMP)
             ++check;
 
-        game_console::program p_test = p;
-        p_test[check].name           = (p_test[check].name == "jmp") ? "nop" : "jmp";
+        // toggle check
+        p[check].op = (p[check].op == game_console::OP_TYPE::NOP) ? game_console::OP_TYPE::JMP
+                                                                  : game_console::OP_TYPE::NOP;
 
-        ++check;
-
-        game_console console{p_test};
+        game_console console{p};
 
         auto [acc, inf_loop] = console.run();
 
@@ -103,26 +100,77 @@ int part2(const game_console::program& p)
             found       = true;
             accumulator = acc;
         }
+
+        // toggle back
+        p[check].op = (p[check].op == game_console::OP_TYPE::NOP) ? game_console::OP_TYPE::JMP
+                                                                  : game_console::OP_TYPE::NOP;
+
+        ++check;
     }
 
     return accumulator;
 }
 
+game_console::program read_input_program(std::istream&& i)
+{
+    std::map<std::string, game_console::OP_TYPE> op_map = {
+        {"nop", game_console::OP_TYPE::NOP},
+        {"jmp", game_console::OP_TYPE::JMP},
+        {"acc", game_console::OP_TYPE::ACC}};
+    ;
+
+    // clang-format off
+    return ranges::getlines(i) 
+        | rv::transform([&op_map](auto&& s) {
+            return game_console::instruction{op_map.at(s.substr(0, 3)), std::stoi(s.substr(4))}; })
+        | ranges::to<std::vector>;
+    // clang-format on
+}
+
+#ifndef UNIT_TESTING
+
 int main()
 {
     fmt::print("Advent of Code 2020 - Day 08\n");
 
-    std::ifstream ifs{"days/day08/input.txt"};
-
-    // clang-format off
-    auto program = ranges::getlines(ifs) 
-        | rv::transform([](auto&& s) {
-            return game_console::instruction{s.substr(0, 3), std::stoi(s.substr(4))}; })
-        | ranges::to<std::vector>;
-    // clang-format on
+    auto program = read_input_program(std::ifstream{"days/day08/input.txt"});
 
     fmt::print("Part 1 Solution: {}\n", part1(program));
-    fmt::print("Part 1 Solution: {}\n", part2(program));
+    fmt::print("Part 1 Solution: {}\n", part2(std::move(program)));
 
     return 0;
 }
+
+#else
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <sstream>
+
+TEST_CASE("Can solve day 8 problems")
+{
+    std::stringstream ss;
+
+    ss << R"(nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6)";
+
+    auto program = read_input_program(std::move(ss));
+
+    REQUIRE(9 == program.size());
+    REQUIRE(game_console::OP_TYPE::ACC == program[5].op);
+    REQUIRE(-99 == program[5].amt);
+
+
+    SECTION("Can solve part 1 example") { REQUIRE(5 == part1(program)); }
+
+    SECTION("Can solve part 2 example") { REQUIRE(8 == part2(program)); }
+}
+
+#endif
