45791e3643c5dabb98235b4a460f77236ab463f6
eb <eb@2bros.dev>
2020-12-08 01:06:01 -0600

    added stubs for day 9

---
+++ days/day09/main.cpp
@@ -0,0 +1,26 @@
+
+#include <fmt/core.h>
+
+#include <fstream>
+
+int part1()
+{
+    return 0;
+}
+
+int part2()
+{
+    return 0;
+}
+
+int main()
+{
+    fmt::print("Advent of Code 2020 - Day 09\n");
+
+    std::ifstream ifs{"days/day09/input.txt"};
+
+    fmt::print("Part 1 Solution: {}\n", part1());
+    fmt::print("Part 1 Solution: {}\n", part2());
+
+    return 0;
+}
