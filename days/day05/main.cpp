495fe960ac3f117197dd345e74a713d6c86ad2c8
eb <eb@2bros.dev>
2020-12-10 21:25:29 -0600

    day 5 refactor and test cleanup

--- days/day05/main.cpp
+++ days/day05/main.cpp
@@ -1,6 +1,3 @@
-
-#include <aoc2020/aoc2020.hpp>
-
 #include <fmt/core.h>
 
 #pragma warning(push)
@@ -11,8 +8,34 @@
 #include <bitset>
 #include <fstream>
 
-using namespace aoc2020;
-using namespace ranges;
+namespace rv = ranges::views;
+
+int calculate_seat_id(std::string_view pass)
+{
+    auto to_bits = [](char c) {
+        if (c == 'F' || c == 'L')
+            return '0';
+        else
+            return '1';
+    };
+
+    return std::bitset<10>(pass | rv::transform(to_bits) | ranges::to<std::string>).to_ulong();
+}
+
+int64_t part1(const std::vector<int>& input)
+{
+    return ranges::max(input);
+}
+
+int64_t part2(const std::vector<int>& input)
+{
+    auto [min_id, max_id] = ranges::minmax(input);
+    auto sum              = ranges::accumulate(input, 0);
+
+    return ((input.size() + 1) * (min_id + max_id) / 2) - sum;
+}
+
+#ifndef UNIT_TESTING
 
 int main()
 {
@@ -20,13 +43,27 @@ int main()
 
     std::ifstream ifs{"days/day05/input.txt"};
 
-    auto seat_ids = getlines(ifs) | views::transform(calculate_seat_id) | to<std::vector<int>>;
+    auto input = ranges::getlines(ifs) | rv::transform(calculate_seat_id)
+                 | ranges::to<std::vector<int>>;
 
-    auto [min_id, max_id] = minmax(seat_ids);
-    auto sum              = accumulate(seat_ids, 0);
-
-    fmt::print("Part 1 Solution: {}\n", max_id);
-    fmt::print("Part 2 Solution: {}\n", ((seat_ids.size() + 1) * (min_id + max_id) / 2) - sum);
+    fmt::print("Part 1 Solution: {}\n", part1(input));
+    fmt::print("Part 2 Solution: {}\n", part2(input));
 
     return 0;
 }
+
+#else
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <sstream>
+
+TEST_CASE("Can calculate seat id from boarding pass", "[day05]")
+{
+    REQUIRE(357 == calculate_seat_id("FBFBBFFRLR"));
+    REQUIRE(567 == calculate_seat_id("BFFFBBFRRR"));
+    REQUIRE(119 == calculate_seat_id("FFFBBBFRRR"));
+    REQUIRE(820 == calculate_seat_id("BBFFBBFRLL"));
+}
+
+#endif
--- include/aoc2020/aoc2020.hpp
+++ include/aoc2020/aoc2020.hpp
@@ -8,13 +8,6 @@
 
 namespace aoc2020 {
 
-std::vector<int> read_int_per_line(std::istream& input);
-
-std::vector<std::pair<int, int>> find_sum_to_pairs(const std::vector<int>& vals, int target);
-
-std::vector<std::tuple<int, int, int>>
-find_sum_to_triplets(const std::vector<int>& vals, int target);
-
 int calculate_row_id(std::string_view boarding_pass);
 
 int calculate_column(std::string_view boarding_pass);
