b1285b6f9bdf5af9687bf264e55aaf672e591974
eb <eb@2bros.dev>
2020-12-10 22:36:57 -0600

    day 6 refactor

--- days/day06/main.cpp
+++ days/day06/main.cpp
@@ -6,57 +6,65 @@
 #include <set>
 #include <string>
 
-using namespace ranges;
+namespace rv = ranges::views;
+
+int64_t part1(const std::vector<std::string>& input)
+{
+    std::set<char> q1;
+    int64_t        sum = 0;
+
+    for (const auto& str : input) {
+        if (str.length() == 0) {
+            sum += q1.size();
+            q1.clear();
+        }
+
+        ranges::insert(q1, str);
+    }
+
+    return sum;
+}
+
+int64_t part2()
+{
+    int64_t sum = 0;
+
+    std::string str;
+    std::string questions;
+
+    std::ifstream ifs{"days/day06/input.txt"};
+
+    std::getline(ifs, questions);
+    ranges::sort(questions);
+
+    while (std::getline(ifs, str)) {
+        ranges::sort(str);
+
+        if (str.length() == 0) {
+            sum += static_cast<int>(questions.size());
+
+            std::getline(ifs, questions);
+            ranges::sort(questions);
+        }
+        else {
+            questions = rv::set_intersection(questions, str) | ranges::to<std::string>;
+        }
+    }
+
+    return sum;
+}
 
 int main()
 {
     fmt::print("Advent of Code 2020 - Day 06\n");
 
-    {
-        std::ifstream  ifs{"days/day06/input.txt"};
-        std::string    str;
-        std::set<char> q1;
-        size_t         part1_sum = 0;
 
-        while (std::getline(ifs, str)) {
-            if (str.length() == 0) {
-                part1_sum += q1.size();
-                q1.clear();
-            }
+    std::ifstream ifs{"days/day06/input.txt"};
 
-            insert(q1, str);
-        }
+    auto input = ranges::getlines(ifs) | ranges::to<std::vector>;
 
-        fmt::print("Part 1 Solution: {}\n", part1_sum);
-    }
-
-    {
-        std::ifstream ifs{"days/day06/input.txt"};
-
-        int part2_sum = 0;
-
-        std::string str;
-        std::string questions;
-
-        std::getline(ifs, questions);
-        sort(questions);
-
-        while (std::getline(ifs, str)) {
-            sort(str);
-
-            if (str.length() == 0) {
-                part2_sum += static_cast<int>(questions.size());
-
-                std::getline(ifs, questions);
-                sort(questions);
-            }
-            else {
-                questions = views::set_intersection(questions, str) | to<std::string>;
-            }
-        }
-
-        fmt::print("Part 2 Solution: {}\n", part2_sum);
-    }
+    fmt::print("Part 1 Solution: {}\n", part1(input));
+    fmt::print("Part 2 Solution: {}\n", part2());
 
     return 0;
 }
