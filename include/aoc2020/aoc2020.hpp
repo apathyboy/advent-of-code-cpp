6d590622ac44663209a55cb1e034d6d4956aa545
eb <eb@2bros.dev>
2020-12-11 04:37:27 -0600

    removed unused code

--- include/aoc2020/aoc2020.hpp
+++ include/aoc2020/aoc2020.hpp
@@ -1,17 +1,2 @@
 
 #pragma once
-
-#include <iosfwd>
-#include <string>
-#include <utility>
-#include <vector>
-
-namespace aoc2020 {
-
-int calculate_row_id(std::string_view boarding_pass);
-
-int calculate_column(std::string_view boarding_pass);
-
-int calculate_seat_id(std::string_view boarding_pass);
-
-} // namespace aoc2020
--- src/aoc2020.cpp
+++ src/aoc2020.cpp
@@ -1,29 +1,2 @@
 
 #include "aoc2020.hpp"
-
-#include <range/v3/all.hpp>
-
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <iostream>
-#include <ranges>
-#include <sstream>
-
-using namespace ranges;
-
-namespace aoc2020 {
-
-int calculate_seat_id(std::string_view pass)
-{
-    auto to_bits = [](char c) {
-        if (c == 'F' || c == 'L')
-            return '0';
-        else
-            return '1';
-    };
-
-    return std::bitset<10>(pass | views::transform(to_bits) | to<std::string>).to_ulong();
-}
-
-} // namespace aoc2020
