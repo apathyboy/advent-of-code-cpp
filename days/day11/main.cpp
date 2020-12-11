afcb340dc0842452f4e339424f88ad0b935078ce
eb <eb@2bros.dev>
2020-12-11 02:50:36 -0600

    part 2 solution brute force

--- days/day11/main.cpp
+++ days/day11/main.cpp
@@ -2,9 +2,31 @@
 #include <range/v3/all.hpp>
 
 #include <fstream>
+#include <optional>
 
 namespace ra = ranges::actions;
 namespace rv = ranges::views;
+
+enum class direction {
+    TOP_LEFT = 0,
+    TOP_MIDDLE,
+    TOP_RIGHT,
+    LEFT,
+    RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_MIDDLE,
+    BOTTOM_RIGHT
+};
+
+const std::vector<direction> ALL_DIRECTIONS = {
+    direction::TOP_LEFT,
+    direction::TOP_MIDDLE,
+    direction::TOP_RIGHT,
+    direction::LEFT,
+    direction::RIGHT,
+    direction::BOTTOM_LEFT,
+    direction::BOTTOM_MIDDLE,
+    direction::BOTTOM_RIGHT};
 
 std::pair<std::vector<char>, int64_t> read_input(std::istream&& input)
 {
@@ -13,32 +35,119 @@ std::pair<std::vector<char>, int64_t> read_input(std::istream&& input)
     return std::make_pair(lines | rv::join | ranges::to<std::vector>, lines[0].length());
 }
 
-bool is_occupied(const std::vector<char>& input, int64_t idx)
+std::optional<int64_t>
+calculate_adjacent_index(int64_t map_size, int64_t stride, int64_t idx, direction d)
 {
+    int64_t                spot_on_line = idx % stride;
+    std::optional<int64_t> adjacent_idx;
 
-    if (idx < 0 || idx > static_cast<int64_t>(input.size() - 1))
-        return false;
+    switch (d) {
+        case direction::TOP_LEFT: {
+            if (spot_on_line > 0)
+                adjacent_idx = idx - stride - 1;
+        } break;
+        case direction::TOP_MIDDLE: {
+            adjacent_idx = idx - stride;
+        } break;
+        case direction::TOP_RIGHT: {
+            if (spot_on_line < stride - 1)
+                adjacent_idx = idx - stride + 1;
+        } break;
+        case direction::LEFT: {
+            if (spot_on_line > 0)
+                adjacent_idx = idx - 1;
+        } break;
+        case direction::RIGHT: {
+            if (spot_on_line < stride - 1)
+                adjacent_idx = idx + 1;
+        } break;
+        case direction::BOTTOM_LEFT: {
+            if (spot_on_line > 0)
+                adjacent_idx = idx + stride - 1;
+        } break;
+        case direction::BOTTOM_MIDDLE: {
+            adjacent_idx = idx + stride;
+        } break;
+        case direction::BOTTOM_RIGHT: {
+            if (spot_on_line < stride - 1)
+                adjacent_idx = idx + stride + 1;
+        } break;
+    }
 
-    return input[idx] == '#';
+    if (adjacent_idx && (adjacent_idx.value() < 0 || adjacent_idx.value() >= map_size))
+        adjacent_idx = std::nullopt;
+
+
+    return adjacent_idx;
 }
 
-int count_occupied_adjacent(const std::vector<char>& input, int64_t idx, int64_t stride)
+std::vector<int64_t> find_adjacent_indexes(int64_t map_size, int64_t stride, int64_t idx)
 {
-    int64_t spot_on_line = idx % stride;
+    auto adjacent_indexes = ALL_DIRECTIONS | rv::transform([map_size, stride, idx](auto dir) {
+                                return calculate_adjacent_index(map_size, stride, idx, dir);
+                            })
+                            | rv::filter([](const auto& idx) { return idx.has_value(); })
+                            | rv::transform([](const auto& idx) { return idx.value(); })
+                            | ranges::to<std::vector>;
 
-    auto top_left   = spot_on_line > 0 ? (is_occupied(input, idx - stride - 1) ? 1 : 0) : 0;
-    auto top_middle = is_occupied(input, idx - stride) ? 1 : 0;
-    auto top_right = spot_on_line < stride - 1 ? (is_occupied(input, idx - stride + 1) ? 1 : 0)
-                                               : 0;
+    return adjacent_indexes;
+}
 
-    auto left  = spot_on_line > 0 ? (is_occupied(input, idx - 1) ? 1 : 0) : 0;
-    auto right = spot_on_line < stride - 1 ? (is_occupied(input, idx + 1) ? 1 : 0) : 0;
+int64_t count_occupied_adjacent(const std::vector<char>& input, int64_t stride, int64_t idx)
+{
+    auto adjacent_indexes = find_adjacent_indexes(input.size(), stride, idx);
 
-    auto bottom_left   = spot_on_line > 0 ? (is_occupied(input, idx + stride - 1) ? 1 : 0) : 0;
-    auto bottom_middle = is_occupied(input, idx + stride) ? 1 : 0;
-    auto bottom_right  = spot_on_line < stride - 1
-                            ? (is_occupied(input, idx + stride + 1) ? 1 : 0)
-                            : 0;
+    return ranges::distance(
+        adjacent_indexes | rv::transform([&input](auto idx) { return input[idx] == '#'; })
+        | rv::filter([](auto b) { return b; }));
+}
+
+bool is_visibly_occupied(
+    const std::vector<char>& input,
+    int64_t                  stride,
+    int64_t                  idx,
+    direction                d)
+{
+    auto adjacent_idx = calculate_adjacent_index(input.size(), stride, idx, d);
+
+    if (!adjacent_idx || input[adjacent_idx.value()] == 'L')
+        return false;
+    if (adjacent_idx && input[adjacent_idx.value()] == '#')
+        return true;
+    else
+        return is_visibly_occupied(input, stride, adjacent_idx.value(), d);
+}
+
+std::vector<int64_t> find_visible_indexes(int64_t map_size, int64_t stride, int64_t idx)
+{
+    auto adjacent_indexes = ALL_DIRECTIONS | rv::transform([map_size, stride, idx](auto dir) {
+                                return calculate_adjacent_index(map_size, stride, idx, dir);
+                            })
+                            | rv::filter([](const auto& idx) { return idx.has_value(); })
+                            | rv::transform([](const auto& idx) { return idx.value(); })
+                            | ranges::to<std::vector>;
+
+    return adjacent_indexes;
+}
+
+
+int count_occupied_visible_adjacent(
+    const std::vector<char>& input,
+    int64_t                  stride,
+    int64_t                  idx)
+{
+    auto top_left   = is_visibly_occupied(input, stride, idx, direction::TOP_LEFT) ? 1 : 0;
+    auto top_middle = is_visibly_occupied(input, stride, idx, direction::TOP_MIDDLE) ? 1 : 0;
+    auto top_right  = is_visibly_occupied(input, stride, idx, direction::TOP_RIGHT) ? 1 : 0;
+
+    auto left  = is_visibly_occupied(input, stride, idx, direction::LEFT) ? 1 : 0;
+    auto right = is_visibly_occupied(input, stride, idx, direction::RIGHT) ? 1 : 0;
+
+    auto bottom_left = is_visibly_occupied(input, stride, idx, direction::BOTTOM_LEFT) ? 1 : 0;
+    auto bottom_middle = is_visibly_occupied(input, stride, idx, direction::BOTTOM_MIDDLE) ? 1
+                                                                                           : 0;
+    auto bottom_right = is_visibly_occupied(input, stride, idx, direction::BOTTOM_RIGHT) ? 1
+                                                                                         : 0;
 
     auto counter = top_left + top_middle + top_right + left + right + bottom_left
                    + bottom_middle + bottom_right;
@@ -70,7 +179,7 @@ int64_t part1(std::vector<char> input, int64_t stride)
                     auto [idx, c] = rng;
 
                     if (c != '.') {
-                        auto occupied_adjacents = count_occupied_adjacent(tmp, idx, stride);
+                        auto occupied_adjacents = count_occupied_adjacent(tmp, stride, idx);
 
                         if (c == 'L' && occupied_adjacents == 0) {
                             c = '#';
@@ -90,9 +199,40 @@ int64_t part1(std::vector<char> input, int64_t stride)
     return current_counter;
 }
 
-int64_t part2()
+int64_t part2(std::vector<char> input, int64_t stride)
 {
-    return 0;
+    int64_t last_counter, current_counter = ranges::count(input, '#');
+
+    do {
+        last_counter = current_counter;
+
+        print_map(input, stride);
+
+        auto tmp = input;
+
+        input = rv::enumerate(input) | rv::transform([&tmp, stride](auto&& rng) {
+                    auto [idx, c] = rng;
+
+                    if (c != '.') {
+                        auto occupied_adjacents =
+                            count_occupied_visible_adjacent(tmp, stride, idx);
+
+                        if (c == 'L' && occupied_adjacents == 0) {
+                            c = '#';
+                        }
+                        else if (c == '#' && occupied_adjacents >= 5) {
+                            c = 'L';
+                        }
+                    }
+
+                    return c;
+                })
+                | ranges::to<std::vector>;
+
+        current_counter = ranges::count(input, '#');
+    } while (last_counter != current_counter);
+
+    return current_counter;
 }
 
 #ifndef UNIT_TESTING
@@ -104,7 +244,7 @@ int main()
     auto [input, stride] = read_input(std::ifstream{"days/day11/input.txt"});
 
     fmt::print("Part 1 Solution: {}\n", part1(input, stride));
-    fmt::print("Part 2 Solution: {}\n", part2());
+    fmt::print("Part 2 Solution: {}\n", part2(input, stride));
 
     return 0;
 }
@@ -118,11 +258,9 @@ int main()
 
 TEST_CASE("Can solve day 11 problems")
 {
-    SECTION("Can solve part 1 example")
-    {
-        std::stringstream ss;
+    std::stringstream ss;
 
-        ss << R"(L.LL.LL.LL
+    ss << R"(L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
 LLLL.LL.LL
@@ -133,12 +271,11 @@ LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL)";
 
-        auto [input, stride] = read_input(std::move(ss));
+    auto [input, stride] = read_input(std::move(ss));
 
-        REQUIRE(37 == part1(input, stride));
-    }
+    SECTION("Can solve part 1 example") { REQUIRE(37 == part1(input, stride)); }
 
-    SECTION("Can solve part 2 example") { REQUIRE(0 == part2()); }
+    SECTION("Can solve part 2 example") { REQUIRE(26 == part2(input, stride)); }
 }
 
 #endif
