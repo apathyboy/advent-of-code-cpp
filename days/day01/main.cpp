b7822cb194948651e9a05890282df80ece2b45e9
eb <eb@2bros.dev>
2020-12-11 04:23:51 -0600

    added day 1 tests

--- days/day01/main.cpp
+++ days/day01/main.cpp
@@ -42,18 +42,52 @@ int part2(const std::vector<int>& input)
     return ranges::front(result);
 }
 
+#ifndef UNIT_TESTING
+
 int main()
 {
     fmt::print("Advent of Code 2020 - Day 01\n");
 
     std::ifstream ifs{"days/day01/input.txt"};
 
-    std::vector<int> input = ranges::getlines(ifs)
-                             | rv::transform([](auto&& s) { return std::stoi(s); })
-                             | ranges::to<std::vector<int>>;
+    // clang-format off
+    auto input = ranges::getlines(ifs) 
+        | rv::transform([](auto&& s) { return std::stoi(s); })
+        | ranges::to<std::vector>;
+    // clang-format on
 
     fmt::print("Part 1 Solution: {}\n", part1(input));
     fmt::print("Part 2 Solution: {}\n", part2(input));
 
     return 0;
 }
+
+#else
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <sstream>
+
+TEST_CASE("Can solve day 2 problems")
+{
+    std::stringstream ss;
+
+    ss << R"(1721
+979
+366
+299
+675
+1456)";
+
+    // clang-format off
+    auto input = ranges::getlines(ss) 
+        | rv::transform([](auto&& s) { return std::stoi(s); })
+        | ranges::to<std::vector>;
+    // clang-format on
+
+    SECTION("Can solve part 1 example") { REQUIRE(514579 == part1(input)); }
+
+    SECTION("Can solve part 2 example") { REQUIRE(241861950 == part2(input)); }
+}
+
+#endif
