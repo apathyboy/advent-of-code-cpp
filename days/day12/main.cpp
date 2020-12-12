759b3c481c1ff37aedfa98a8c9dc8e8a29e1fe25
eb <eb@2bros.dev>
2020-12-12 07:56:19 -0600

    refactor day 12

--- days/day12/main.cpp
+++ days/day12/main.cpp
@@ -6,118 +6,53 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <vector>
 
 namespace ra = ranges::actions;
 namespace rv = ranges::views;
 
-struct action {
-    char type;
+struct instruction {
+    char dir;
     int  amount;
 };
 
-std::vector<action> read_input(std::istream&& input)
+std::map<char, glm::ivec2> directions = {
+    {'N', glm::ivec2{0, 1}},
+    {'E', glm::ivec2{1, 0}},
+    {'S', glm::ivec2{0, -1}},
+    {'W', glm::ivec2{-1, 0}}};
+
+std::vector<instruction> read_input(std::istream&& input)
 {
     // clang-format off
     return ranges::getlines(input) 
-        | rv::transform([](auto&& s) { return action{s[0], std::stoi(s.substr(1))}; })
+        | rv::transform([](auto&& s) { return instruction{s[0], std::stoi(s.substr(1))}; })
         | ranges::to<std::vector>;
     // clang-format on
 }
 
-char turn(action step, char current_heading)
+int navigate(const std::vector<instruction>& input, glm::ivec2 heading, bool waypoint = false)
 {
-    auto dirs = std::array{'N', 'W', 'S', 'E'};
+    glm::ivec2 ship_position = {0, 0};
 
-    auto start      = ranges::distance(dirs.begin(), ranges::find(dirs, current_heading));
-    auto turn_steps = (step.amount / 90) * (step.type == 'L' ? 1 : -1);
-
-    return dirs[(start + turn_steps) % dirs.size()];
-}
-
-glm::ivec2 move_step(char heading, int amount)
-{
-    glm::ivec2 move = {0, 0};
-
-    switch (heading) {
-        case 'N': {
-            move += glm::ivec2{0, amount};
-        } break;
-        case 'S': {
-            move += glm::ivec2{0, -amount};
-        } break;
-        case 'E': {
-            move += glm::ivec2{amount, 0};
-        } break;
-        case 'W': {
-            move += glm::ivec2{-amount, 0};
-        } break;
-    }
-
-    return move;
-}
-
-glm::ivec2 move_around(action step, glm::ivec2 current_pos)
-{
-    for (int i = 0; i < step.amount / 90; ++i) {
-        std::swap(current_pos.x, current_pos.y);
-        current_pos *= (step.type == 'L') ? glm::ivec2{-1, 1} : glm::ivec2{1, -1};
-    }
-
-    return current_pos;
-}
-
-int64_t part1(const std::vector<action>& input)
-{
-    char current_heading = 'E';
-
-    glm::ivec2 pos = {0, 0};
-
-    for (const auto& step : input) {
-        switch (step.type) {
-            case 'N':
-            case 'S':
-            case 'E':
-            case 'W': {
-                pos += move_step(step.type, step.amount);
-            } break;
-            case 'L':
-            case 'R': {
-                current_heading = turn(step, current_heading);
-            } break;
-            case 'F': {
-                pos += move_step(current_heading, step.amount);
-            } break;
+    for (const auto& instr : input) {
+        if (instr.dir == 'L' || instr.dir == 'R') {
+            ranges::for_each(rv::iota(0, instr.amount / 90), [&heading, &instr](auto) {
+                std::tie(heading.x, heading.y) = std::make_tuple(
+                    heading.y * ((instr.dir == 'L') ? -1 : 1),
+                    heading.x * ((instr.dir == 'R') ? -1 : 1));
+            });
+        }
+        else if (instr.dir == 'F') {
+            ship_position += heading * instr.amount;
+        }
+        else {
+            (waypoint ? heading : ship_position) += directions.at(instr.dir) * instr.amount;
         }
     }
 
-    return std::abs(pos.x) + std::abs(pos.y);
-}
-
-int64_t part2(const std::vector<action>& input)
-{
-    glm::ivec2 waypoint_pos = {10, 1};
-    glm::ivec2 ship_pos     = {0, 0};
-
-    for (const auto& step : input) {
-        switch (step.type) {
-            case 'N':
-            case 'S':
-            case 'E':
-            case 'W': {
-                waypoint_pos += move_step(step.type, step.amount);
-            } break;
-            case 'L':
-            case 'R': {
-                waypoint_pos = move_around(step, waypoint_pos);
-            } break;
-            case 'F': {
-                ship_pos += waypoint_pos * step.amount;
-            } break;
-        }
-    }
-
-    return std::abs(ship_pos.x) + std::abs(ship_pos.y);
+    return std::abs(ship_position.x) + std::abs(ship_position.y);
 }
 
 #ifndef UNIT_TESTING
@@ -128,8 +63,8 @@ int main()
 
     auto input = read_input(std::ifstream{"days/day12/puzzle.in"});
 
-    fmt::print("Part 1 Solution: {}\n", part1(input));
-    fmt::print("Part 2 Solution: {}\n", part2(input));
+    fmt::print("Part 1 Solution: {}\n", navigate(input, directions.at('E')));
+    fmt::print("Part 2 Solution: {}\n", navigate(input, {10, 1}, true));
 
     return 0;
 }
@@ -152,9 +87,9 @@ F11)";
 
     auto input = read_input(std::move(ss));
 
-    REQUIRE(input[0].type == 'F');
+    REQUIRE(input[0].dir == 'F');
     REQUIRE(input[0].amount == 10);
-    REQUIRE(input[3].type == 'R');
+    REQUIRE(input[3].dir == 'R');
     REQUIRE(input[3].amount == 90);
 }
 
@@ -171,9 +106,9 @@ F11)";
 
     auto input = read_input(std::move(ss));
 
-    SECTION("Can solve part 1 example") { REQUIRE(25 == part1(input)); }
+    SECTION("Can solve part 1 example") { REQUIRE(25 == navigate(input, directions.at('E'))); }
 
-    SECTION("Can solve part 2 example") { REQUIRE(286 == part2(input)); }
+    SECTION("Can solve part 2 example") { REQUIRE(286 == navigate(input, {10, 1}, true)); }
 }
 
 #endif
