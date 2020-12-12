e9de06bae5e8b727ce99efce028356c5c4ec5078
eb <eb@2bros.dev>
2020-12-12 00:10:00 -0600

    day 12 part 1 solution

--- days/day12/main.cpp
+++ days/day12/main.cpp
@@ -1,14 +1,91 @@
 #include <fmt/core.h>
+#include <glm/vec2.hpp>
 #include <range/v3/all.hpp>
 
+#include <array>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
 namespace ra = ranges::actions;
 namespace rv = ranges::views;
 
-int64_t part1()
+struct action {
+    char type;
+    int amount;
+};
+
+std::vector<action> read_input(std::istream&& input)
 {
-    return 0;
+    return ranges::getlines(input) | rv::transform([](auto && s) {
+        return action{s[0], std::stoi(s.substr(1))};
+    })
+    | ranges::to<std::vector>;
+}
+
+char turn(action step, char current_heading)
+{
+    auto dirs = std::array{'N', 'W', 'S', 'E'};
+
+    if (step.type == 'R') {
+        dirs = std::array{'N', 'E', 'S', 'W'};
+    }
+
+    auto start = ranges::distance(dirs.begin(), ranges::find(dirs, current_heading));
+
+    auto turn_steps = step.amount / 90;
+
+    auto rng = dirs | rv::cycle | rv::slice(start, start + turn_steps + 1);
+
+    return ranges::back(rng);
+}
+
+glm::ivec2 move_step(char heading, int amount)
+{
+    glm::ivec2 move = {0, 0};
+    switch (heading) {
+            case 'N': {
+                move += glm::ivec2{0, amount};
+            } break;
+            case 'S': {
+                move += glm::ivec2{0, -amount};
+            } break;
+            case 'E': {
+                move += glm::ivec2{amount, 0};
+            } break;
+            case 'W': {
+                move += glm::ivec2{-amount, 0};
+            } break;    
+    }
+
+    return move;
+}
+
+int64_t part1(const std::vector<action>& input)
+{
+    char current_heading = 'E';
+
+    glm::ivec2 pos = {0, 0};
+
+    for (const auto& step : input) {
+        switch (step.type) {
+            case 'N':
+            case 'S':
+            case 'E':
+            case 'W': {
+                pos += move_step(step.type, step.amount);
+            } break;
+            case 'L':
+            case 'R': {
+                current_heading = turn(step, current_heading);
+            } break;
+            case 'F': {
+                pos += move_step(current_heading, step.amount);
+            } break;
+        }
+    }
+
+    return std::abs(pos.x) + std::abs(pos.y);
 }
 
 int64_t part2()
@@ -22,9 +99,9 @@ int main()
 {
     fmt::print("Advent of Code 2020 - Day 12\n");
 
-    std::ifstream{"days/day12/puzzle.in"};
+    auto input = read_input(std::ifstream{"days/day12/puzzle.in"});
 
-    fmt::print("Part 1 Solution: {}\n", part1());
+    fmt::print("Part 1 Solution: {}\n", part1(input));
     fmt::print("Part 2 Solution: {}\n", part2());
 
     return 0;
@@ -36,14 +113,38 @@ int main()
 #include <catch2/catch.hpp>
 #include <sstream>
 
+TEST_CASE("Can read input to actions")
+{
+    std::stringstream ss;
+
+    ss << R"(F10
+N3
+F7
+R90
+F11)";
+
+    auto input = read_input(std::move(ss));
+
+    REQUIRE(input[0].type == 'F');
+    REQUIRE(input[0].amount == 10);
+    REQUIRE(input[3].type == 'R');
+    REQUIRE(input[3].amount == 90);
+}
+
 
 TEST_CASE("Can solve day 12 problems")
 {
     std::stringstream ss;
 
-    ss << R"()";
+    ss << R"(F10
+N3
+F7
+R90
+F11)";
 
-    SECTION("Can solve part 1 example") { REQUIRE(0 == part1()); }
+    auto input = read_input(std::move(ss));
+
+    SECTION("Can solve part 1 example") { REQUIRE(25 == part1(input)); }
 
     SECTION("Can solve part 2 example") { REQUIRE(0 == part2()); }
 }
