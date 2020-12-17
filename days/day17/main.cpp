a453c48f423893f52e45a395e8df3add317f31f2
eb <eb@2bros.dev>
2020-12-17 04:02:20 -0600

    day 17 part 2 solution

--- days/day17/main.cpp
+++ days/day17/main.cpp
@@ -10,34 +10,60 @@
 namespace rs = ranges;
 namespace rv = ranges::views;
 
-using grid_t = std::unordered_map<glm::ivec3, bool>;
-
-void print_grid(const grid_t& g)
-{
-    (g);
-}
+using grid_t  = std::unordered_map<glm::ivec3, bool>;
+using grid2_t = std::unordered_map<glm::ivec4, bool>;
 
 grid_t read_input_grid(std::istream&& input)
 {
-    int x = 0, y = 0, z = 0;
+    glm::ivec3 expanse = {0, 0, 0};
 
     std::string tmp;
     grid_t      grid;
 
     while (std::getline(input, tmp)) {
-        x = 0;
+        expanse.x = 0;
         for (auto c : tmp) {
-            grid[glm::ivec3{x, y, z}] = (c == '#');
-            ++x;
+            grid[expanse] = (c == '#');
+            ++expanse.x;
         }
-        ++y;
+        ++expanse.y;
     }
 
     for (int tmp_z = -1; tmp_z <= 1; ++tmp_z) {
-        for (int tmp_y = -1; tmp_y <= y; ++tmp_y) {
-            for (int tmp_x = -1; tmp_x <= x; ++tmp_x) {
+        for (int tmp_y = -1; tmp_y <= expanse.y; ++tmp_y) {
+            for (int tmp_x = -1; tmp_x <= expanse.x; ++tmp_x) {
                 glm::ivec3 pos = {tmp_x, tmp_y, tmp_z};
                 if (!grid.contains(pos)) { grid[pos] = false; }
+            }
+        }
+    }
+
+    return grid;
+}
+
+grid2_t read_input_grid2(std::istream&& input)
+{
+    glm::ivec4 expanse = {0, 0, 0, 0};
+
+    std::string tmp;
+    grid2_t     grid;
+
+    while (std::getline(input, tmp)) {
+        expanse.x = 0;
+        for (auto c : tmp) {
+            grid[expanse] = (c == '#');
+            ++expanse.x;
+        }
+        ++expanse.y;
+    }
+
+    for (int tmp_w = -1; tmp_w <= 1; ++tmp_w) {
+        for (int tmp_z = -1; tmp_z <= 1; ++tmp_z) {
+            for (int tmp_y = -1; tmp_y <= expanse.y; ++tmp_y) {
+                for (int tmp_x = -1; tmp_x <= expanse.x; ++tmp_x) {
+                    glm::ivec4 pos = {tmp_x, tmp_y, tmp_z, tmp_w};
+                    if (!grid.contains(pos)) { grid[pos] = false; }
+                }
             }
         }
     }
@@ -60,7 +86,29 @@ int count_active_neighbors(grid_t& grid, glm::ivec3 p)
     return active;
 }
 
+int count_active_neighbors2(grid2_t& grid, glm::ivec4 p)
+{
+    int active = 0;
+
+    for (int w = p.w - 1; w <= p.w + 1; ++w) {
+        for (int z = p.z - 1; z <= p.z + 1; ++z) {
+            for (int y = p.y - 1; y <= p.y + 1; ++y) {
+                for (int x = p.x - 1; x <= p.x + 1; ++x) {
+                    if (p != glm::ivec4{x, y, z, w}) { active += grid[glm::ivec4{x, y, z, w}] ? 1 : 0; }
+                }
+            }
+        }
+    }
+
+    return active;
+}
+
 int64_t count_active(const grid_t& g)
+{
+    return rs::count(g | rv::values, true);
+}
+
+int64_t count_active2(const grid2_t& g)
 {
     return rs::count(g | rv::values, true);
 }
@@ -86,6 +134,27 @@ void run_cycle(grid_t& grid)
     }
 }
 
+void run_cycle2(grid2_t& grid)
+{
+    auto tmp = grid;
+
+    // loop through tmp and check against grid
+    for (auto& p : tmp) {
+        auto active_neighbors = count_active_neighbors2(grid, p.first);
+
+        if (p.second) {
+            if (active_neighbors != 2 && active_neighbors != 3) { p.second = false; }
+        }
+        else {
+            if (active_neighbors == 3) { p.second = true; }
+        }
+    }
+
+    for (auto& p : grid) {
+        p.second = tmp[p.first];
+    }
+}
+
 int64_t part1(grid_t input)
 {
     for (int i : rv::iota(0, 6)) {
@@ -95,9 +164,13 @@ int64_t part1(grid_t input)
     return count_active(input);
 }
 
-int64_t part2()
+int64_t part2(grid2_t input)
 {
-    return 0;
+    for (int i : rv::iota(0, 6)) {
+        run_cycle2(input);
+    }
+
+    return count_active2(input);
 }
 
 #ifndef UNIT_TESTING
@@ -108,10 +181,8 @@ int main()
 
     std::string input_path = "days/day17/puzzle.in";
 
-    auto input = read_input_grid(std::ifstream{input_path});
-
-    fmt::print("Part 1 Solution: {}\n", part1(input));
-    fmt::print("Part 2 Solution: {}\n", part2());
+    fmt::print("Part 1 Solution: {}\n", part1(read_input_grid(std::ifstream{input_path})));
+    fmt::print("Part 2 Solution: {}\n", part2(read_input_grid2(std::ifstream{input_path})));
 
     return 0;
 }
@@ -226,18 +297,18 @@ TEST_CASE("Can solve part 1 example")
 ..#
 ###)";
 
-    auto input = read_input_grid(std::move(ss));
-
-    REQUIRE(112 == part1(input));
+    REQUIRE(112 == part1(read_input_grid(std::move(ss))));
 }
 
 TEST_CASE("Can solve part 2 example")
 {
     std::stringstream ss;
 
-    ss << R"()";
+    ss << R"(.#.
+..#
+###)";
 
-    REQUIRE(0 == part2());
+    REQUIRE(848 == part2(read_input_grid2(std::move(ss))));
 }
 
 #endif
