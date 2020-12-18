d7e0e22c5196f2b80c16ff2886d470a276e5d1fe
eb <eb@2bros.dev>
2020-12-18 13:29:28 -0600

    day 18 part 2 solution

--- days/day18/main.cpp
+++ days/day18/main.cpp
@@ -52,6 +52,55 @@ int64_t solve_expression(std::istream& expr)
     return solution;
 }
 
+
+int64_t solve_advanced_expression(std::istream& expr)
+{
+    int64_t solution = 0;
+
+    int64_t num;
+    char    op = ' ', dummy;
+
+    while (!(expr >> std::ws).eof()) {
+        char c = static_cast<char>((expr >> std::ws).peek());
+        switch (c) {
+            case '(': {
+                expr >> dummy;
+
+                num = solve_advanced_expression(expr);
+
+                if (op == '+') solution += num;
+                else if (op == '*')
+                    solution *= solve_advanced_expression(expr);
+                else
+                    solution = num;
+            } break;
+            case ')': {
+                expr >> dummy;
+                return solution;
+            } break;
+            case '+': {
+                expr >> op;
+            } break;
+            case '*': {
+                expr >> op;
+                solution *= solve_advanced_expression(expr);
+                return solution;
+            } break;
+            default: {
+                expr >> num;
+
+                if (op == '+') solution += num;
+                else if (op == '*')
+                    solution *= solve_advanced_expression(expr);
+                else
+                    solution = num;
+            } break;
+        }
+    }
+
+    return solution;
+}
+
 int64_t part1(std::istream&& input)
 {
     auto solutions = rs::getlines(input) | rv::transform([](auto&& s) {
@@ -62,9 +111,14 @@ int64_t part1(std::istream&& input)
     return rs::accumulate(solutions, int64_t{0});
 }
 
-int64_t part2()
+int64_t part2(std::istream&& input)
 {
-    return 0;
+    auto solutions = rs::getlines(input) | rv::transform([](auto&& s) {
+                         std::stringstream ss{s | rs::to<std::string>};
+                         return solve_advanced_expression(ss);
+                     });
+
+    return rs::accumulate(solutions, int64_t{0});
 }
 
 #ifndef UNIT_TESTING
@@ -76,7 +130,7 @@ int main()
     std::string input_path = "days/day18/puzzle.in";
 
     fmt::print("Part 1 Solution: {}\n", part1(std::ifstream{input_path}));
-    fmt::print("Part 2 Solution: {}\n", part2());
+    fmt::print("Part 2 Solution: {}\n", part2(std::ifstream{input_path}));
 
     return 0;
 }
@@ -122,6 +176,48 @@ TEST_CASE("Can solve expressions that start with nestings")
     REQUIRE(13632 == solve_expression(ss));
 }
 
+TEST_CASE("Can solve simple expression using advanced math")
+{
+    std::string       e = "1 + 2 * 3 + 4 * 5 + 6";
+    std::stringstream ss{e};
+    REQUIRE(231 == solve_advanced_expression(ss));
+}
+
+TEST_CASE("Can solve expression with 1 nesting using advanced math")
+{
+    {
+        std::string       e = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
+        std::stringstream ss{e};
+        REQUIRE(1445 == solve_advanced_expression(ss));
+    }
+    {
+        std::string       e = "2 * 3 + (4 * 5)";
+        std::stringstream ss{e};
+        REQUIRE(46 == solve_advanced_expression(ss));
+    }
+}
+
+TEST_CASE("Can solve advanced expressions with multiple nestings")
+{
+    {
+        std::string       e = "1 + (2 * 3) + (4 * (5 + 6))";
+        std::stringstream ss{e};
+        REQUIRE(51 == solve_advanced_expression(ss));
+    }
+    {
+        std::string       e = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+        std::stringstream ss{e};
+        REQUIRE(23340 == solve_advanced_expression(ss));
+    }
+}
+
+TEST_CASE("Can solve advanced expressions with multiple subsequent multiplications")
+{
+    std::string       e = "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))";
+    std::stringstream ss{e};
+    REQUIRE(669060 == solve_advanced_expression(ss));
+}
+
 TEST_CASE("Can solve part 1 example")
 {
     std::stringstream ss;
@@ -137,9 +233,11 @@ TEST_CASE("Can solve part 2 example")
 {
     std::stringstream ss;
 
-    ss << R"()";
+    ss << R"(2 * 3 + (4 * 5)
+5 + (8 * 3 + 9 + 3 * 4 * 3)
+5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)))";
 
-    REQUIRE(0 == part2());
+    REQUIRE(670551 == part2(std::move(ss)));
 }
 
 #endif
