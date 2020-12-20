4ffec26a1c503f302ec9ee85dc5c2a42a167dd25
eb <eb@2bros.dev>
2020-12-19 23:39:03 -0600

    parse 20 input

--- days/day20/main.cpp
+++ days/day20/main.cpp
@@ -3,8 +3,33 @@
 
 #include <fstream>
 
+#include <iostream>
+
 namespace rs = ranges;
 namespace rv = ranges::views;
+
+struct tile {
+    int                            id;
+    std::vector<std::vector<char>> image_data;
+};
+
+int parse_tile_id(const std::string& s)
+{
+    return std::stoi(s.substr(5, s.length() - 1));
+}
+
+std::vector<tile> read_input(std::istream&& input)
+{
+    return rs::getlines(input) | rv::split("") | rv::transform([](auto&& rng) {
+               auto v = rng | rv::transform([](auto&& s) { return s | rs::to<std::string>; })
+                        | rs::to_vector;
+
+               tile t{parse_tile_id(v[0]), rv::tail(v) | rs::to<std::vector<std::vector<char>>>};
+
+               return t;
+           })
+           | rs::to_vector;
+}
 
 int64_t part1()
 {
@@ -24,7 +49,7 @@ int main()
 
     std::string input_path = "days/day20/puzzle.in";
 
-    std::ifstream ifs{input_path};
+    auto input = read_input(std::ifstream{input_path});
 
     fmt::print("Part 1 Solution: {}\n", part1());
     fmt::print("Part 2 Solution: {}\n", part2());
