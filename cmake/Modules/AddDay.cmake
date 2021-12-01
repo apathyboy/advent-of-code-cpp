37557f5cd02b624c9ba9375f2d60e25af82c68b7
eb <eb@eb2.co>
2021-12-01 11:47:59 -0700

    group together app and test targets
    grouping together the targets allows for shorter build
    times when working on individual days

--- cmake/Modules/AddDay.cmake
+++ cmake/Modules/AddDay.cmake
@@ -9,12 +9,12 @@ function(AddDay)
         ${ARGN} # arguments of the function to parse, here we take the all original ones
     )
 
-    set(APP_NAME ${PUZZLE_YEAR}_${PUZZLE_DAY})
+    set(APP_NAME_PREFIX ${PUZZLE_YEAR}_${PUZZLE_DAY})
 
-    add_executable(${APP_NAME} ${PUZZLE_DAY}/main.cpp)
+    add_executable(${APP_NAME_PREFIX}_app ${PUZZLE_DAY}/main.cpp)
 
     target_compile_options(
-        ${APP_NAME}
+        ${APP_NAME_PREFIX}_app
         PRIVATE $<$<CXX_COMPILER_ID:MSVC>:
                 -wd4201
                 -wd4996
@@ -24,23 +24,23 @@ function(AddDay)
                 -wd6330>)
 
     target_link_libraries(
-        ${APP_NAME}
+        ${APP_NAME_PREFIX}_app
         PRIVATE aoc
                 fmt::fmt
                 range-v3::meta
                 ${PUZZLE_LIBS})
 
-    add_executable(${APP_NAME}_tests ${PUZZLE_DAY}/main.cpp)
+    add_executable(${APP_NAME_PREFIX}_tests ${PUZZLE_DAY}/main.cpp)
 
     target_link_libraries(
-        ${APP_NAME}_tests
+        ${APP_NAME_PREFIX}_tests
         PRIVATE aoc
                 fmt::fmt
                 range-v3::meta
                 ${PUZZLE_LIBS})
 
     target_compile_options(
-        ${APP_NAME}_tests
+        ${APP_NAME_PREFIX}_tests
         PRIVATE $<$<CXX_COMPILER_ID:MSVC>:
                 -wd4201
                 -wd4996
@@ -49,7 +49,10 @@ function(AddDay)
                 -wd4702 # TODO range-v3 error
                 -wd6330>) # TODO catch2 error
 
-    target_compile_definitions(${APP_NAME}_tests PRIVATE UNIT_TESTING)
+    target_compile_definitions(${APP_NAME_PREFIX}_tests PRIVATE UNIT_TESTING)
 
-    catch_discover_tests(${APP_NAME}_tests)
+    catch_discover_tests(${APP_NAME_PREFIX}_tests)
+
+    add_custom_target(${APP_NAME_PREFIX} DEPENDS ${APP_NAME_PREFIX}_app
+                                                 ${APP_NAME_PREFIX}_tests)
 endfunction()
