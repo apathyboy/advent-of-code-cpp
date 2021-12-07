5195748ecb5b93c44524029903f9ce1333d92378
eb <eb@eb2.co>
2021-12-07 02:40:27 -0700

    day07 solutions

---
+++ puzzles/2021/day07/main.cpp
@@ -0,0 +1,87 @@
+#include <aoc/aoc.hpp>
+
+#include <fmt/core.h>
+#include <range/v3/all.hpp>
+
+#include <fstream>
+
+namespace rs = ranges;
+namespace rv = ranges::views;
+namespace ra = ranges::actions;
+
+int part1(std::vector<int> input)
+{
+    int fuel = std::numeric_limits<int>::max();
+
+    for (int test_pos : input | rv::unique) {
+        auto tmp = rs::accumulate(
+            input | rv::transform([test_pos](int pos) { return std::abs(test_pos - pos); }),
+            0);
+
+        if (tmp < fuel) fuel = tmp;
+    }
+
+    return fuel;
+}
+
+int part2(std::vector<int> input)
+{
+    int fuel = std::numeric_limits<int>::max();
+
+    auto t = rs::minmax(input);
+
+    for (int test_pos : rv::closed_iota(t.min, t.max)) {
+        auto tmp = rs::accumulate(
+            input | rv::transform([test_pos](int pos) {
+                auto tmp = rs::accumulate(rv::closed_iota(0, std::abs(test_pos - pos)), 0);
+                return tmp;
+            }),
+            0);
+
+        if (tmp < fuel) fuel = tmp;
+    }
+
+    return fuel;
+}
+
+#ifndef UNIT_TESTING
+
+int main()
+{
+    fmt::print("Advent of Code 2021 - Day 07\n");
+
+    std::ifstream ifs{"puzzle.in"};
+    auto          input = aoc::split_line_by<int>(ifs);
+
+    fmt::print("Part 1 Solution: {}\n", part1(input));
+    fmt::print("Part 2 Solution: {}\n", part2(input));
+}
+
+#else
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <sstream>
+
+
+TEST_CASE("Can solve part 1 example")
+{
+    std::stringstream ss;
+    ss << R"(16,1,2,0,4,2,7,1,2,14)";
+
+    auto input = aoc::split_line_by<int>(ss);
+
+    REQUIRE(37 == part1(input));
+}
+
+TEST_CASE("Can solve part 2 example")
+{
+    std::stringstream ss;
+    ss << R"(16,1,2,0,4,2,7,1,2,14)";
+
+    auto input = aoc::split_line_by<int>(ss);
+
+    REQUIRE(168 == part2(input));
+}
+
+#endif
--- puzzles/day_template/main.cpp
+++ puzzles/day_template/main.cpp
@@ -1,18 +1,56 @@
+#include <aoc/aoc.hpp>
+
+#include <fmt/core.h>
+#include <range/v3/all.hpp>
+
+#include <fstream>
+
+namespace rs = ranges;
+namespace rv = ranges::views;
+namespace ra = ranges::actions;
+
+int part1()
+{
+    return 0;
+}
+
+int part2()
+{
+    return 0;
+}
 
 #ifndef UNIT_TESTING
 
 int main()
 {
-    return 0;
+    fmt::print("Advent of Code 2021 - Day XX\n");
+
+    std::ifstream ifs{"puzzle.in"};
+
+    fmt::print("Part 1 Solution: {}\n", part1());
+    fmt::print("Part 2 Solution: {}\n", part2());
 }
 
 #else
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <sstream>
 
-TEST_CASE("Can solve part 1 example") {}
+TEST_CASE("Can solve part 1 example")
+{
+    std::stringstream ss;
+    ss << R"()";
 
-TEST_CASE("Can solve part 2 example") {}
+    REQUIRE(0 == part1());
+}
+
+TEST_CASE("Can solve part 2 example")
+{
+    std::stringstream ss;
+    ss << R"()";
+
+    REQUIRE(0 == part2());
+}
 
 #endif
